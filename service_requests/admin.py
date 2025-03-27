ecdba7b75eefa5b49702df0ae14bc8f7628055c0
AdityaNik <aadityanikam2004@gmail.com>
2025-03-27 12:27:47 +0530

    init

---
+++ service_requests/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import ServiceRequest  # Import your model
+
+admin.site.register(ServiceRequest)  # Register the model
---
+++ service_requests/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from service_requests.views import ServiceRequestViewSet
+
+router = DefaultRouter()
+router.register(r'service-requests', ServiceRequestViewSet, basename='service-request')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+]
